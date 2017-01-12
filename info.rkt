0f063fc268efe2c37a154391edabb970200abe75
Jack Firth <jackhfirth@gmail.com>
2017-01-11 17:10:38 -0800

    Add package info

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang info
+(define collection "net")
+(define deps '("fancy-app"
+               "reprovide-lang"
+               "compose-app"
+               "functional-lib"
+               "megaparsack-lib"
+               "base"))
+(define build-deps '("scribble-lib"
+                     "scribble-text-lib"
+                     "rackunit-lib"))
