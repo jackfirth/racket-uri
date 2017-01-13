983ff7980d739c6373faf36fd7b374df2f5a949c
Jack Firth <jackhfirth@gmail.com>
2017-01-13 00:42:11 -0800

    Add empty authority helper
    And empty authority is either the empty registered name, the IP address
    127.0.0.1, or the dns address “localhost”

--- uri/private/authority.rkt
+++ uri/private/authority.rkt
@@ -18,9 +18,12 @@
   [dns-reg-name (-> dns-address? dns-reg-name?)]
   [dns-reg-name? predicate/c]
   [dns-reg-name-address (-> dns-reg-name? dns-address?)]
+  [empty-reg-name empty-reg-name?]
+  [empty-reg-name? predicate/c]
   [host? predicate/c]
   [net-host? predicate/c]
   [net-authority? predicate/c]
+  [net-authority/empty? predicate/c]
   [reg-name? predicate/c]
   [userinfo? predicate/c]
   [user+password-info (->* (string?) (#:password (or/c string? #f))
@@ -32,6 +35,7 @@
 (require compose-app/fancy-app
          net/dns/base
          net/ip/base
+         racket/function
          racket/tcp
          "struct.rkt")
 
@@ -44,7 +48,9 @@
   (make-user+password-info user password))
 
 (struct/supertype-only reg-name)
+(struct/singleton-subtype empty-reg-name reg-name)
 (struct dns-reg-name reg-name (address) #:transparent #:omit-define-syntaxes)
+
 (define host? (or/c ipv4-address? reg-name?))
 (define net-host? (or/c ipv4-address? dns-reg-name?))
 
@@ -55,10 +61,19 @@
   (make-authority host userinfo port))
 
 (define (authority/c #:host [host-pred host?]
-                     #:userinfo [userinfo-pred userinfo?])
+                     #:userinfo [userinfo-pred userinfo?]
+                     #:port [port-pred (const #t)])
   (and/c authority?
          (host-pred .. authority-host)
-         (or/c userinfo-pred #f .. authority-userinfo)))
+         (userinfo-pred .. authority-userinfo)
+         (port-pred .. authority-port)))
 
 (define net-authority?
-  (authority/c #:host net-host? #:userinfo user+password-info?))
+  (authority/c #:host net-host? #:userinfo (or/c user+password-info? #f)))
+
+(define net-authority/empty?
+  (authority/c #:host (or/c empty-reg-name?
+                            localhost?
+                            (dns-localhost? .. dns-reg-name-address))
+               #:userinfo #f
+               #:port not))
