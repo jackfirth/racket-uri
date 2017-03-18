fa6fefbbf6c06025253d641e3498429bba96739d
Jack Firth <jackhfirth@gmail.com>
2017-03-17 20:16:43 -0700

    Add dependency

--- info.rkt
+++ info.rkt
@@ -6,7 +6,8 @@
                "functional-lib"
                "megaparsack-lib"
                ("base" #:version "6.5.0.900")))
-(define build-deps '("racket-doc"
+(define build-deps '("megaparsack-doc"
+                     "racket-doc"
                      "net-doc"
                      "scribble-lib"
                      "scribble-text-lib"
